848ef059a4c5e4b30b4ba6cd533d47423901a1bd
unknown <hejunior@FHMOB2097.fh.com.br>
2015-03-25 18:44:52 -0300

    To be continued...

---
+++ scripts/app.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var app = angular
+  .module('TaskMeApp', [
+    'ngAnimate',
+    'ngResource',    
+    'ngRoute',    
+    'firebase'
+  ])
+
+  .constant('FURL', 'https://task-me.firebaseio.com/')
+
+  .config(function ($routeProvider) {
+    $routeProvider      
+      .when('/', {
+        templateUrl: 'views/main.html'        
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html'
+      })
+      .when('/register', {
+        templateUrl: 'views/register.html'
+      })
+      .when('/post', {
+        templateUrl: 'views/post.html',
+        controller: 'TaskController'
+      })
+      .when('/edit/:taskId', {
+        templateUrl: 'views/edit.html',
+        controller: 'TaskController'
+      })
+      .when('/browse', {
+        templateUrl: 'views/browse.html',
+        controller: 'TaskController'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
