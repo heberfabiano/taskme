ab484888166363d97864b9fc656c0cfa434054b5
unknown <hejunior@FHMOB2097.fh.com.br>
2015-03-26 14:39:38 -0300

    comments added

---
+++ scripts/controllers/browse.js
@@ -0,0 +1,47 @@
+'use strict';
+
+app.controller('BrowseController', function($scope, $routeParams, toaster, Task, Auth, Comment) {
+
+	$scope.searchTask = '';
+	$scope.tasks = Task.all;
+	$scope.signedIn = Auth.signedIn;
+	$scope.listMode = true;
+
+	$scope.user = Auth.user;
+
+	if($routeParams.taskId) {
+		var task = Task.getTask($routeParams.taskId).$asObject();
+		$scope.listMode = false;
+		setSelectedTask(task);
+	}
+
+	function setSelectedTask(task) {
+		$scope.selectedTask = task;
+
+		if($scope.signedIn()) {
+			$scope.isTaskCreator = Task.isCreator;
+			$scope.isOpen = Task.isOpen;
+		}
+
+		$scope.comments = Comment.comments(task.$id);
+	}
+
+	$scope.cancelTask = function(taskId) {
+		Task.cancelTask(taskId).then(function() {
+			toaster.pop('success', 'Tarefa cancelada com sucesso.');
+		});
+	}
+
+	$scope.addComment = function() {
+		var comment = {
+			content: $scope.content,
+			name: $scope.user.profile.name,
+			gravatar: $scope.user.profile.gravatar
+		};
+
+		Comment.addComment($scope.selectedTask.$id, comment).then(function () {
+			$scope.content = '';
+		});
+	}
+
+})
